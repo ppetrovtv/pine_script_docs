4161b4c4dc168b888261034417cc07c4f0311f9d
vvolkov <vvolkov@tradingview.com>
2019-02-27 18:13:17 +0300

    Added requirements.txt and rtd theme

--- source/conf.py
+++ source/conf.py
@@ -79,12 +79,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# Read more https://alabaster.readthedocs.io/en/latest/
-html_theme = 'alabaster'
-html_theme_options = {
-    'logo': 'Pine_Script_logo.png',
-    # 'show_related': True, # shows previous/next page link on the sidebar
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
