4abed37437eddd4ba2a758a998b43c66947c1ab3
vvolkov <vvolkov@tradingview.com>
2019-06-03 21:17:28 +0300

    Trying guzzle theme

--- source/conf.py
+++ source/conf.py
@@ -86,16 +86,26 @@ highlight_language = 'pine'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
 
-# Hint: To do local theme development clone the sphinx_rtd_theme git repo and symlink it to `_themes/sphinx_rtd_theme`
-html_theme_path = ['_themes/sphinx_rtd_theme/']
+import guzzle_sphinx_theme
+html_translator_class = 'guzzle_sphinx_theme.HTMLTranslator'
+html_theme = 'guzzle_sphinx_theme'
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+# Hint: To do local theme development clone the guzzle_sphinx_theme git repo and symlink it to `_themes/guzzle_sphinx_theme`
+# html_theme_path = ['_themes/guzzle_sphinx_theme/guzzle_sphinx_theme/']
+
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    '**': ['logo-text.html', 'globaltoc.html', 'searchbox.html']
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
