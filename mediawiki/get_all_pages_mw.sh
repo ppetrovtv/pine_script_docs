3ca468e3abfc611f8b75f04692ea4e624c50b1b8
vvolkov <vvolkov@tradingview.com>
2019-06-10 16:59:22 +0300

    Removed useless trash, updated Readme

--- mediawiki/get_all_pages_mw.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p pages_mw
-while read p; do
-  curl -G "https://www.tradingview.com/wiki/static/index.php" --data-urlencode "title=$p" --data-urlencode "action=raw" > "pages_mw/$p.mw"
-done < pages_titles.txt
